a6de90fe17b4d62500e4e366e8ff41c9516032ca
Ankad1985 <109212214+Ankad1985@users.noreply.github.com>
2024-03-02 02:49:06 +0300

    Update settings.py

--- HW6/settings.py
+++ HW6/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
         'HOST': os.getenv('MYSQL_HOST'),
         'OPTIONS': {
-	        'init_command': "SET NAMES 'utf8mb4';SETsql_mode='STRICT_TRANS_TABLES'",
+	        'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
         },
     }
